8a7a21836f665467e2d0ab8f377d94b4c369be0d
Franck STAUFFER <franck.stauffer@rbs.fr>
2011-01-04 16:01:10 +0100

    [REFACTOR] call to request context

--- lib/services/ModuleService.class.php
+++ lib/services/ModuleService.class.php
@@ -51,15 +51,11 @@ class comment_ModuleService extends ModuleBaseService
 	}
 	
 	/**
+	 * @deprecated
 	 * @return String
 	 */
 	public function getIp()
 	{
-		$ip = isset($_SERVER['HTTP_X_FORWARDED_FOR']) ? $_SERVER['HTTP_X_FORWARDED_FOR'] : null;
-		if (f_util_StringUtils::isEmpty($ip))
-		{
-			$ip = isset($_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : null;
-		}
-		return $ip;
+		return RequestContext::getInstance()->getClientIp();
 	}
 }
